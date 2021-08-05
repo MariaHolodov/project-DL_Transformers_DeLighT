1b9df2a7bcf4651ab6372e66efa8cb9b6618daf3
kerengaiger <keren.gaiger@gmail.com>
2021-08-05 08:43:46 +0300

    dataset: add pbar update

--- data/dataset.py
+++ data/dataset.py
@@ -20,6 +20,7 @@ class Dataset(object):
         with tqdm(desc='loading all image ids features', unit='it', total=len(image_ids)) as pbar:
             for image_id in image_ids:
                 self.detections_data[image_id] = f['%d_features' % image_id][()]
+                pbar.update()
 
     def collate_fn(self):
         def collate(batch):
