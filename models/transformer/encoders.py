e3b0c9e4ebb93eaf85ba0c3e00fbd811dbabf9c9
kerengaiger <keren.gaiger@gmail.com>
2021-08-10 18:10:28 +0300

    encoder: pass args to MultiHeadAttention

--- models/transformer/encoders.py
+++ models/transformer/encoders.py
@@ -7,11 +7,12 @@ from models.transformer.attention import MultiHeadAttention
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, d_model=64, d_k=8, d_v=8, h=1, d_ff=2048, dropout=.1, identity_map_reordering=False,
+    def __init__(self, d_model=512, d_middle=[1024], d_o=256, d_k=64, d_v=64, h=1, d_ff=2048, dropout=.1, identity_map_reordering=False,
                  attention_module=None, attention_module_kwargs=None):
         super(EncoderLayer, self).__init__()
         self.identity_map_reordering = identity_map_reordering
-        self.mhatt = MultiHeadAttention(d_model, d_k, d_v, h, dropout, identity_map_reordering=identity_map_reordering,
+
+        self.mhatt = MultiHeadAttention(d_model, d_middle, d_o, d_k, d_v, h, dropout, identity_map_reordering=identity_map_reordering,
                                         attention_module=attention_module,
                                         attention_module_kwargs=attention_module_kwargs)
         self.pwff = PositionWiseFeedForward(d_model, d_ff, dropout, identity_map_reordering=identity_map_reordering)
@@ -23,12 +24,12 @@ class EncoderLayer(nn.Module):
 
 
 class MultiLevelEncoder(nn.Module):
-    def __init__(self, N, padding_idx, d_model=64, d_k=8, d_v=8, h=1, d_ff=2048, dropout=.1,
+    def __init__(self, N, padding_idx, d_model=512, d_middle=[512], d_o=256, d_k=64, d_v=64, h=1, d_ff=128, dropout=.1,
                  identity_map_reordering=False, attention_module=None, attention_module_kwargs=None):
         super(MultiLevelEncoder, self).__init__()
         self.d_model = d_model
         self.dropout = dropout
-        self.layers = nn.ModuleList([EncoderLayer(d_model, d_k, d_v, h, d_ff, dropout,
+        self.layers = nn.ModuleList([EncoderLayer(d_model, d_middle, d_o, d_k, d_v, h, d_ff, dropout,
                                                   identity_map_reordering=identity_map_reordering,
                                                   attention_module=attention_module,
                                                   attention_module_kwargs=attention_module_kwargs)
@@ -42,6 +43,7 @@ class MultiLevelEncoder(nn.Module):
         outs = []
         out = input
         for l in self.layers:
+
             out = l(out, out, out, attention_mask, attention_weights)
             outs.append(out.unsqueeze(1))
 
