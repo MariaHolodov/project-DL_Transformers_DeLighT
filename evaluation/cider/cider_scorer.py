2a06be735b41be70215b834c67a20ad7da8ea55d
MariaHolodov <maria.holod@gmail.com>
2021-08-10 14:40:34 +0300

    Update cider_scorer.py

--- evaluation/cider/cider_scorer.py
+++ evaluation/cider/cider_scorer.py
@@ -129,7 +129,7 @@ class CiderScorer(object):
             for n in range(self.n):
                 # ngram
                 for (ngram,count) in vec_hyp[n].items():
-                    pritn(ngram)
+                    print(ngram)
                     # vrama91 : added clipping
                     val[n] += min(vec_hyp[n][ngram], vec_ref[n][ngram]) * vec_ref[n][ngram]
 
