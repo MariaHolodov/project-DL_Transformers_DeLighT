14ab5b43e96975ee9473c9ab9209bae42d3ee3db
kerengaiger <keren.gaiger@gmail.com>
2021-08-11 07:21:14 +0300

    tokenizer: download stopwords

--- evaluation/tokenizer.py
+++ evaluation/tokenizer.py
@@ -8,41 +8,34 @@
 # Last Modified : Thu Mar 19 09:53:35 2015
 # Authors : Hao Fang <hfang@uw.edu> and Tsung-Yi Lin <tl483@cornell.edu>
 
-import os
-import subprocess
-import tempfile
 import nltk
-import string
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-from nltk import pos_tag
+
 
 class PTBTokenizer(object):
     """Python wrapper of Stanford PTBTokenizer"""
 
-    corenlp_jar = 'stanford-corenlp-3.4.1.jar'
     punctuations = ["''", "'", "``", "`", "-LRB-", "-RRB-", "-LCB-", "-RCB-", \
                     ".", "?", "!", ",", ":", "-", "--", "...", ";"]
 
     @classmethod
     def tokenize(cls, corpus):
-        cmd = ['java', '-cp', cls.corenlp_jar, \
-                'edu.stanford.nlp.process.PTBTokenizer', \
-                '-preserveLines', '-lowerCase']
-
         if isinstance(corpus, list) or isinstance(corpus, tuple):
             if isinstance(corpus[0], list) or isinstance(corpus[0], tuple):
                 corpus = {i:c for i, c in enumerate(corpus)}
             else:
                 corpus = {i: [c, ] for i, c in enumerate(corpus)}
 
-        # prepare data for PTB Tokenizer
         tokenized_corpus = {}
         image_id = [k for k, v in list(corpus.items()) for _ in range(len(v))]
         sentences = '\n'.join([c.replace('\n', ' ') for k, v in corpus.items() for c in v])
 
-        #TODO: tokenizer
+        # download stopwords
+        nltk.download('stopwords')
+
+
         sentences_lst = sentences.split('\n')
         stop_words = stopwords.words('english')
         porter = PorterStemmer()
