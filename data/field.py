75862550d1c9b25b67c717ae407bbbe5d3b1c0d3
MariaHolodov <maria.holod@gmail.com>
2021-08-22 01:16:12 +0300

    f

--- data/field.py
+++ data/field.py
@@ -127,7 +127,7 @@ class ImageDetectionsField(RawField):
                 f = h5py.File(self.detections_path, 'r')
                 precomp_data = f['%d_features' % image_id][()]
             precomp_data = np.concatenate([precomp_data,vc_f1],axis=1)
-                if self.sort_by_prob:
+            if self.sort_by_prob:
                     precomp_data = precomp_data[np.argsort(np.max(f['%d_cls_prob' % image_id][()], -1))[::-1]]
         except KeyError:
             warnings.warn('Could not find detections for %d' % image_id)
