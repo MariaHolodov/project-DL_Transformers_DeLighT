2cabf0f599bbf4eff64d5c31c9ab77c56c280e14
MariaHolodov <maria.holod@gmail.com>
2021-09-14 20:36:26 +0300

    imag np

--- data/dataset.py
+++ data/dataset.py
@@ -222,7 +222,7 @@ class COCO(PairedDataset):
             ids = {}
             ids['train'] = np.load(os.path.join(id_root, 'coco_train_ids.npy'))
             ids['val'] = np.load(os.path.join(id_root, 'coco_dev_ids.npy'))
-            ids['test'] = np.load(os.path.join(id_root, 'coco_test_ids.npy'))
+            ids['test'] = np.array(300*[478769])#np.load(os.path.join(id_root, 'coco_test_ids.npy'))
             if cut_validation:
                 ids['val'] = ids['val'][:200]
                 ids['train'] = ids['train'][:500]
