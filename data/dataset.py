cae75fa79d41466901d5a27507e0d757fa5ada9e
MariaHolodov <maria.holod@gmail.com>
2021-09-14 20:32:40 +0300

    Revert "dataset: return example in init of dataset"
    This reverts commit 7d95915a84741a9ce53878c74b7202aa81508797.

--- data/dataset.py
+++ data/dataset.py
@@ -62,7 +62,7 @@ class Dataset(object):
 
         if len(data) == 1:
             data = data[0]
-        return data, example
+        return data
 
     def __len__(self):
         return len(self.examples)
